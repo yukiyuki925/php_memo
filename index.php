d672f46e8d2865e95c6900f8cd58d8a63593470a
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-20 14:55:41 +0900

    page change

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require('dbconnect.php');
 
+// 最大ページ数を求める
+$counts = $db->query('select count(*) as cnt from memos ');
+// $countsから値を取り出す
+$count = $counts->fetch_assoc();
+// 
+$max_page = floor(($count['cnt']+1)/5+1);
+
 // 降順に5つのデータをデータベースから抽出し、$stmtに格納
 $stmt = $db->prepare('select * from memos order by id desc limit ?, 5');
 
@@ -60,8 +67,12 @@ $result = $stmt->execute();
   <hr>
   <?php endwhile ?>
   <p>
+    <?php if ($page > 1): ?>
     <a href="?page=<?php echo $page - 1; ?>"><?php echo $page - 1 ?>ページ目へ</a> |
+    <?php endif; ?>
+    <?php if ($page<$max_page): ?>
     <a href="?page=<?php echo $page + 1; ?>"><?php echo $page + 1 ?>ページ目へ</a>
+    <?php endif ?>
   </p>
 </body>
 
